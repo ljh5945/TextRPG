441585b64979cb1059e4f845f7ec4977cb04b5a7
ljh5945 <ljh594545@gmail.com>
2024-01-08 15:36:19 +0900

    스파르타던전

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -41,9 +41,10 @@ namespace SpartaDungeon
             Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 인벤토리");
             Console.WriteLine("3. 상점");
-            Console.WriteLine("4. 휴식하기");
+            Console.WriteLine("4. 던전입장");
+            Console.WriteLine("5. 휴식하기");
             Console.WriteLine();
-            int input = CheckValidInput(1, 4);
+            int input = CheckValidInput(1, 5);
             switch (input)
             {
                 case 1:
@@ -56,8 +57,152 @@ namespace SpartaDungeon
                     DisplayShop();
                     break;
                 case 4:
+                    EntranceDungeon();
+                    break;
+                case 5:
                     TakeRest();
                     break;
+
+            }
+        }
+
+        private static void EntranceDungeon()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("던전입장");
+            Console.ResetColor();
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine("1. 쉬운 던전    ㅣ방어력 5 이상 권장");
+            Console.WriteLine("2. 일반 던전    ㅣ방어력 11 이상 권장");
+            Console.WriteLine("3. 어려운 던전  ㅣ방어력 17 이상 권장");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            int input = CheckValidInput(0, 3);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+                case 1:
+                    EasyDungeon();
+                    break;
+                case 2:
+                    NormalDungeon();
+                    break;
+                case 3:
+                    HardDungeon();
+                    break;
+            }
+        }
+
+        private static void HardDungeon()
+        {
+            Random rand = new Random();
+            int d = player.Def - 17;
+            int r = rand.Next(20, 36);
+            int win = rand.Next(0, 10);
+            int bonus = rand.Next((int)player.Atk, (int)player.Atk * 2);
+            if ((player.Hp - r + d) > 0 && (d >= 0 || win < 6))    //방어력이 권장 방어력보다 높으면 무조건 클리어, 낮으면 60% 확률로 클리어
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("던전 클리어");
+                Console.ResetColor();
+                Console.WriteLine("축하합니다!!");
+                Console.WriteLine("어려운 던전을 클리어 하였습니다.");
+                Console.WriteLine("");
+                Console.WriteLine("[탐험 결과]");
+                Console.WriteLine($"체력 {player.Hp} -> {player.Hp - r + d}");
+                Console.WriteLine($"Gold {player.Gold} G -> {player.Gold + 2500 + 2500 * bonus / 100} G");
+                player.TakeDamage(r - d);
+                player.Gold += 2500 + 2500 * bonus / 100;
+                Console.WriteLine("0. 나가기");
+                Console.WriteLine();
+                int input = CheckValidInput(0, 0);
+                switch (input)
+                {
+                    case 0:
+                        EntranceDungeon();
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("던전 실패");
+                Console.ResetColor();
+                Console.WriteLine("");
+                Console.WriteLine("[탐험 결과]");
+                Console.WriteLine($"체력 {player.Hp} -> {player.Hp - player.Hp / 2}");
+                player.TakeDamage(player.Hp / 2);
+            }
+        }
+
+        private static void NormalDungeon()
+        {
+            Random rand = new Random();
+            int d = player.Def - 11;
+            int r = rand.Next(20, 36);
+            int win = rand.Next(0, 10);
+            int bonus = rand.Next((int)player.Atk, (int)player.Atk * 2);
+            if ((player.Hp - r + d) > 0 && (d >= 0 || win < 6))    //방어력이 권장 방어력보다 높으면 무조건 클리어, 낮으면 60% 확률로 클리어
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("던전 클리어");
+                Console.ResetColor();
+                Console.WriteLine("축하합니다!!");
+                Console.WriteLine("일반 던전을 클리어 하였습니다.");
+                Console.WriteLine("");
+                Console.WriteLine("[탐험 결과]");
+                Console.WriteLine($"체력 {player.Hp} -> {player.Hp - r + d}");
+                Console.WriteLine($"Gold {player.Gold} G -> {player.Gold + 1800 + 1800 * bonus / 100} G");
+                player.TakeDamage(r - d);
+                player.Gold += 1800 + 1800 * bonus / 100;
+                Console.WriteLine("0. 나가기");
+                Console.WriteLine();
+                int input = CheckValidInput(0, 0);
+                switch (input)
+                {
+                    case 0:
+                        EntranceDungeon();
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("던전 실패");
+                Console.ResetColor();
+                Console.WriteLine("");
+                Console.WriteLine("[탐험 결과]");
+                Console.WriteLine($"체력 {player.Hp} -> {player.Hp - player.Hp / 2}");
+                player.TakeDamage(player.Hp / 2);
+            }
+        }
+
+        private static void EasyDungeon()
+        {
+            Random rand = new Random();
+            int d = player.Def - 5;
+            int r = rand.Next(20, 36);
+            int win = rand.Next(0, 10);
+            int bonus = rand.Next((int)player.Atk, (int)player.Atk * 2);
+            if ((player.Hp - r + d) > 0 && (d >= 0 || win < 6))
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("던전 클리어");
+                Console.ResetColor();
+                Console.WriteLine("축하합니다!!");
+                Console.WriteLine("쉬운 던전을 클리어 하였습니다.");
+                Console.WriteLine();
+                Console.WriteLine("[탐험 결과]");
+                Console.WriteLine($"체력 {player.Hp} -> {player.Hp - r + d}");
+                Console.WriteLine($"Gold {player.Gold} G -> {player.Gold + 1000 + 1000 * bonus / 100} G");
 
             }
         }
@@ -451,7 +596,10 @@ namespace SpartaDungeon
             return curItem.isEquipped;
         }
 
-        
+        internal void TakeDamage(int damage)
+        {
+            this.Hp -= damage;
+        }
     }
     public class Item
     {
